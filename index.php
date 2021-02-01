249552f226a6ef40fc84f5f88e105ea38e050d3b
BessonMikael <besson.mikael04@gmail.com>
2021-02-01 09:49:26 +0100

    exo 165

--- index.php
+++ index.php
@@ -9,9 +9,21 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
+const NOMBRES1 = 12;
+const NOMBRES2 = 2;
+
+echo "le resultat de mes 2 constante est : ".NOMBRES1 * NOMBRES2."<br>";
+
+const MON_TEXTE = "C'est cool PHP";
+
+echo "le calcul de mes constante donne :".(NOMBRES1 + NOMBRES2) * strlen(MON_TEXTE)."<br>";
 
 
 /**
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
+
+foreach($_SERVER as $parm => $value){
+    echo "$parm = '$value'\n"."<br>";
+}
